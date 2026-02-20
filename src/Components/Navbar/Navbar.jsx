297cf5bdd83b0ffcd6c3a10e40091bf991a6bb23
himarchi1928 <himarchigiftsandtoys@gmail.com>
2026-02-20 10:35:46 +0530

    added gh-pages and navbar css reponsive to mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,26 +5,22 @@ function Navbar()  {
     return (<div>
       
 
-        <div className='nav' style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "10px 20px",
-      background: "#fdb531",
-      color: "white"
-    }}>
+        <div className='nav'>
+           <div>
+            <Link to = '/' style={{ color: "white", textDecoration: "none" }}><h2>HIMARCHI TRAVELS</h2></Link>
+           </div>
 
-           
-            <Link to = '/' style={{ color: "white", textDecoration: "none" }}> <h2>HIMARCHI TRAVELS</h2></Link>
+            <div>
             <h3>
-          <div style={{ display: "flex", gap: "20px" }}>
-            <Link to = '/' style={{ color: "white", textDecoration: "none" }}>Home</Link>
-            <Link to = '/AboutUs' style={{ color: "white", textDecoration: "none" }}>About Us</Link>
-            <Link to = '/Gallary' style={{ color: "white", textDecoration: "none" }}>Gallary</Link>
-            <Link to = '/ContactUs' style={{ color: "white", textDecoration: "none" }}>Contact Us</Link>
-            <Link to = '/Services'style={{ color: "white", textDecoration: "none" }}>Services</Link>
-          </div>
-          </h3>
+              <div className="options" style={{ display: "flex", gap: "20px" }}>
+                <Link to = '/' style={{ color: "white", textDecoration: "none" }}>Home</Link>
+                <Link to = '/AboutUs' style={{ color: "white", textDecoration: "none" }}>About Us</Link>
+                <Link to = '/Gallary' style={{ color: "white", textDecoration: "none" }}>Gallary</Link>
+                <Link to = '/ContactUs' style={{ color: "white", textDecoration: "none" }}>Contact Us</Link>
+                <Link to = '/Services'style={{ color: "white", textDecoration: "none" }}>Services</Link>
+              </div>
+            </h3>
+            </div>
         
      
         </div>
